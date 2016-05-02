cb3beafdb635cfa9eda0547d839cc7e60ecb2cc4
ruiqingqiu <raymond.qiu1993@gmail.com>
2016-05-02 12:17:17 -0700

    Initial Commit

---
+++ V.I.CTests/V_I_CTests.swift
@@ -0,0 +1,36 @@
+//
+//  V_I_CTests.swift
+//  V.I.CTests
+//
+//  Created by Ruiqing Qiu on 5/2/16.
+//  Copyright © 2016 Ruiqing Qiu. All rights reserved.
+//
+
+import XCTest
+@testable import V_I_C
+
+class V_I_CTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
